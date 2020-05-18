b6c56df43bbe710166208ce3f967b90d2efa9ef6
James Cahill <jcahill773@gmail.com>
2020-05-18 20:36:22 +0100

    [Security] Fix bug where listener would never be called

--- src/main/java/com/dumbdogdiner/betterwhitelist/listeners/PlayerEventListener.java
+++ src/main/java/com/dumbdogdiner/betterwhitelist/listeners/PlayerEventListener.java
@@ -1,13 +1,13 @@
 package com.dumbdogdiner.betterwhitelist.listeners;
 
 import com.dumbdogdiner.betterwhitelist.BaseClass;
-import com.dumbdogdiner.betterwhitelist.BetterWhitelistBungee;
 import com.dumbdogdiner.betterwhitelist.utils.MojangUser;
 import com.dumbdogdiner.betterwhitelist.utils.UsernameValidator;
 
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
@@ -19,19 +19,23 @@ public class PlayerEventListener implements Listener, BaseClass {
     /**
      * Check whether players are allowed to log in.
      */
-    public void onPostLoginEvent(ProxiedPlayer player) {
+    public void onPostLogin(PostLoginEvent event) {
+    	ProxiedPlayer player = event.getPlayer();
+    	
         if (getConfig().getBoolean("disableUuidChecking")) {
-            BetterWhitelistBungee.getInstance().getLogger()
-                    .info(getConfig().getString("lang.console.postLogin.uuidCheckingDisabled"));
+            getLogger().info(getConfig().getString("lang.console.postLogin.uuidCheckingDisabled"));
             return;
         }
         
-        // User has bypass permission.
-        if (player.hasPermission("betterwhitelist.bypass")) return;
-
         MojangUser user = UsernameValidator.getUser(player);
+        
+        // User has bypass permission.
+        if (player.hasPermission("betterwhitelist.bypass")) {
+        	getLogger().info(String.format("'%s' (UUID %s) has a bypass!", user.name, user.id));
+        	return;
+        };
 
-        BetterWhitelistBungee.getInstance().getLogger().info(String.format(getConfig().getString("lang.console.postLogin.checkingUuid"), user.id));
+        getLogger().info(String.format(getConfig().getString("lang.console.postLogin.checkingUuid"), user.id));
 
         if (getSQL().getDiscordIDFromMinecraft(user.id) == null) {
         	// User is not whitelisted.
